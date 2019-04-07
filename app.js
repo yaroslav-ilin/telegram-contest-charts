8f3d99ecc854e286dc5f8f40f7deb038e1ef3270
Yaroslav <nilfalse@yandex.com>
2019-04-07 16:23:17 +0200

    preview window [1st interation]

--- app.js
+++ app.js
@@ -12,12 +12,10 @@ function formatDate (date) {
   return MONTHS[date.getMonth()] + ' ' + date.getDate();
 }
 
-function createNode (parent, tag, attrs) {
-  attrs || (attrs = []);
+function createNode (parent, tag, clsName) {
   var el = document.createElement(tag);
-  for (var i = 0; i < attrs.length; i++) {
-    var attr = attrs[i];
-    el.setAttribute(attr[0], attr[1]);
+  if (clsName) {
+    el.className = clsName;
   }
   parent.appendChild(el);
   return el;
@@ -33,7 +31,7 @@ var listen = (function () {
     });
     window.addEventListener('testPassive', null, opts);
     window.removeEventListener('testPassive', null, opts);
-  } catch (e) {}
+  } catch (ignore) {}
   var passiveArgs = supportsPassive ? { passive: true } : false;
 
   return function listen (node, evt, callback, tryPassive) {
@@ -44,28 +42,11 @@ var listen = (function () {
   };
 }());
 
-// theme
-
-
-var isNightTheme = localStorage.getItem('theme') === 'night';
-function setTheme (isNightTheme) {
-  if (isNightTheme) {
-    document.body.classList.add('night');
-    btnTheme.innerText = 'Switch to Day Mode';
-    localStorage.setItem('theme', 'night');
-  } else {
-    document.body.classList.remove('night');
-    btnTheme.innerText = 'Switch to Night Mode';
-    localStorage.removeItem('theme');
-  }
-}
-setTheme(isNightTheme);
-
 // components
 
 
 function Checkboxes (parent, items, callback) {
-  var host = createNode(parent, 'form', [ ['class', 'checkboxes'] ]);
+  var host = createNode(parent, 'form', 'checkboxes');
   var timeout = null;
 
   function each (cb) {
@@ -90,34 +71,33 @@ function Checkboxes (parent, items, callback) {
   }
   function handleMouseUp (evt) {
     if (timeout === null) {
-      evt.preventDefault();
+      if (evt.cancelable) {
+        evt.preventDefault();
+      }
     } else {
       clearTimeout(timeout);
     }
   }
 
   function createCheckbox (item) {
-    var view = createNode(host, 'label', [
-      ['class', 'checkbox'], ['style', 'color: ' + item[1]]
-    ]);
+    var view = createNode(host, 'label', 'checkbox');
+    view.style.color = item[1];
     listen(view, 'mousedown', handleMouseDown);
     listen(view, 'touchstart', handleMouseDown, true);
     listen(view, 'touchend', handleMouseUp);
     listen(view, 'click', handleMouseUp);
 
-    // <input type="checkbox" checked="checked" name="{name}" />
-    listen(createNode(view, 'input', [
-      ['type', 'checkbox'], ['checked', 'checked'], ['name', item[0]]
-    ]), 'change', handleCheck);
+    var inp = createNode(view, 'input');
+    inp.type = 'checkbox';
+    inp.checked = true;
+    inp.name = item[0];
+    listen(inp, 'change', handleCheck);
 
-    createNode(view, 'div', [ ['class', 'checkbox__bg'] ]);
+    createNode(view, 'div', 'checkbox__bg');
 
-    // <span class="checkbox__checkmark" style="color: {color}"></span>
-    createNode(view, 'span', [
-      ['class', 'checkbox__checkmark']
-    ]);
-    // <span class="checkbox__label">{name}</span>
-    createNode(view, 'span', [ ['class', 'checkbox__label'] ]).innerText = item[2];
+    createNode(view, 'span', 'checkbox__checkmark');
+    createNode(view, 'span', 'checkbox__label')
+      .innerText = item[2];
   }
 
   each(createCheckbox);
@@ -125,25 +105,195 @@ function Checkboxes (parent, items, callback) {
 
 
 function Chart (parent, data) {
-  var view = createNode(parent, 'div', [ ['class', 'chart'] ]);
+  var view = createNode(parent, 'div', 'chart');
 }
 
 
-function Preview (parent, data) {
-  var view = createNode(parent, 'div', [ ['class', 'preview'] ]);
+function Preview (parent, axis) {
+  var view = createNode(parent, 'div', 'preview');
 
-  var pan = createNode(view, 'div', [ ['class', 'pan'] ]);
-  createNode(pan, 'div', [ ['class', 'pan_left'] ]);
-  createNode(pan, 'div', [ ['class', 'pan_right'] ]);
+  var pan = createNode(view, 'div', 'pan');
+  var panL = createNode(pan, 'div', 'pan_left');
+  var panR = createNode(pan, 'div', 'pan_right');
+
+  var panning = null;
+  var zoomingL = null;
+  var zoomingR = null;
+
+  var minSize = 10;
+  var offsetLeft = 0;
+  var offsetRight = 0;
+
+  function syncPan () {
+    var size = 100 - offsetLeft - offsetRight;
+    pan.style.left = offsetLeft + '%';
+    pan.style.width = size + '%';
+  }
+
+  function createGesture (capturedPageX, totalWidth) {
+    return function (fn, extra) {
+      return function (pageX) {
+        var deltaX = pageX - capturedPageX;
+        var deltaPercent = deltaX / totalWidth * 100;
+
+        fn.call(this, deltaPercent, extra);
+
+        syncPan();
+      };
+    };
+  }
+
+  function setLeft (delta, capturedLeft) {
+    offsetLeft = Math.max(
+      0,
+      Math.min(
+        100 - offsetRight - minSize,
+        capturedLeft + delta
+      )
+    );
+  }
+  function setRight (delta, capturedRight) {
+    offsetRight = Math.max(
+      0,
+      Math.min(
+        100 - offsetLeft - minSize,
+        capturedRight - delta
+      )
+    );
+  }
+  function setLeftAndRight (delta, captured) {
+    var capturedLeft = captured[0];
+    var capturedSize = captured[1];
+
+    var capturedRight = 100 - capturedLeft - capturedSize;
+    var limitedDelta = Math.max(
+      -capturedLeft,
+      Math.min(
+        delta,
+        capturedRight
+      )
+    );
+
+    setLeft(limitedDelta, capturedLeft);
+    setRight(limitedDelta, capturedRight);
+  }
+
+  pan.onmousedown = function (evt) {
+    if (!zoomingL && !zoomingR) {
+      var gesture = createGesture(evt.pageX, view.getBoundingClientRect().width);
+      panning = gesture(
+        setLeftAndRight,
+        [ offsetLeft, 100 - offsetLeft - offsetRight ]
+      );
+    }
+  };
+  listen(pan, 'touchstart', function (evt) {
+    if (!zoomingL && !zoomingR) {
+      var gesture = createGesture(evt.changedTouches[0].pageX, view.getBoundingClientRect().width);
+      panning = gesture(
+        setLeftAndRight,
+        [ offsetLeft, 100 - offsetLeft - offsetRight ]
+      );
+    }
+  }, true);
+  panL.onmousedown = function (evt) {
+    evt.stopPropagation();
+    if (!panning) {
+      var gesture = createGesture(evt.pageX, view.getBoundingClientRect().width);
+      zoomingL = gesture(setLeft, offsetLeft);
+    }
+  };
+  listen(panL, 'touchstart', function (evt) {
+    evt.stopPropagation();
+    if (!panning) {
+      var gesture = createGesture(evt.changedTouches[0].pageX, view.getBoundingClientRect().width);
+      zoomingL = gesture(setLeft, offsetLeft);
+    }
+  }, true);
+  panR.onmousedown = function (evt) {
+    evt.stopPropagation();
+    if (!panning) {
+      var gesture = createGesture(evt.pageX, view.getBoundingClientRect().width);
+      zoomingR = gesture(setRight, offsetRight);
+    }
+  };
+  listen(panR, 'touchstart', function (evt) {
+    evt.stopPropagation();
+    if (!panning) {
+      var gesture = createGesture(evt.changedTouches[0].pageX, view.getBoundingClientRect().width);
+      zoomingR = gesture(setRight, offsetRight);
+    }
+  }, true);
+
+  var unlistenMove = listen(document, 'mousemove', function (evt) {
+    if (panning) {
+      panning(evt.pageX);
+      evt.preventDefault();
+    }
+    if (zoomingL) {
+      zoomingL(evt.pageX);
+      evt.preventDefault();
+    }
+    if (zoomingR) {
+      zoomingR(evt.pageX);
+      evt.preventDefault();
+    }
+  });
+  pan.ontouchmove = function (evt) {
+    if (panning) {
+      panning(evt.changedTouches[0].pageX);
+      evt.preventDefault();
+    }
+  };
+  panL.ontouchmove = function (evt) {
+    if (zoomingL) {
+      zoomingL(evt.changedTouches[0].pageX);
+      evt.preventDefault();
+    }
+  };
+  panR.ontouchmove = function (evt) {
+    if (zoomingR) {
+      zoomingR(evt.changedTouches[0].pageX);
+      evt.preventDefault();
+    }
+  };
+
+  var unlistenUp = listen(document, 'mouseup', function (evt) {
+    panning = null;
+    zoomingL = null;
+    zoomingR = null;
+  }, true);
+  listen(pan, 'touchend', function (evt) {
+    panning = null;
+  }, true);
+  listen(panL, 'touchend', function (evt) {
+    evt.stopPropagation();
+    zoomingL = null;
+  }, true);
+  listen(panR, 'touchend', function (evt) {
+    evt.stopPropagation();
+    zoomingR = null;
+  }, true);
+
+  syncPan();
+
+  this.dispose = function () {
+    unlistenUp();
+    unlistenMove();
+  };
 }
 
 
-function ChartScreen (parent, data) {
-  var view = createNode(parent, 'article', [ ['class', 'screen'] ]);
+function ChartScreen (parent, data, idx) {
+  var view = createNode(parent, 'article', 'screen');
+
+  var header = createNode(view, 'header', 'header');
+  var title = createNode(header, 'h1', 'title');
+  title.innerText = 'Chart #' + String(idx + 1)
 
   new Chart(view, data);
 
-  new Preview(view, data);
+  new Preview(view, data.columns[0].slice(1));
 
   var columns = [];
   for (var i = 0; i < data.columns.length; i++) {
@@ -168,7 +318,7 @@ xhr.onreadystatechange = function () {
     console.log(json);
 
     for (var i = 0; i < json.length; i++) {
-      new ChartScreen(root, json[i]);
+      new ChartScreen(root, json[i], i);
     }
   }
 };
